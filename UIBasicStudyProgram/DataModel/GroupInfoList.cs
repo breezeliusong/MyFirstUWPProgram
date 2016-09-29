52d10fa395d33e16b302bfe57a5da8338bec32ff
liusong <liusong@V-DOXIE-W10>
2016-09-29 18:01:21 +0800

    9.29

---
+++ UIBasicStudyProgram/DataModel/GroupInfoList.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ListViewSample.Model
+{
+    public class GroupInfoList : List<object>
+    {
+        public object Key { get; set; }
+    }
+}
--- UIBasicStudyProgram/MainPage.xaml.cs
+++ UIBasicStudyProgram/MainPage.xaml.cs
@@ -7,16 +7,19 @@ using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using ListViewSample.Model;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
+
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
 namespace UIBasicStudyProgram
 {
+
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
